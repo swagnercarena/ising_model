faf8d6f20296cab8b0dad5bc50cafa7e374c2fde
Sebastian Wagner-Carena <sebwagner@DNa1c07b0.SUNet>
2019-03-12 03:26:52 -0700

    added sh file

---
+++ param_search.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --partition=iric,hns,normal
+#SBATCH --ntasks=1 #Number of CPUS
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --time=0-01:00:00  # time (HH:MM:SS)
+#SBATCH -o /home/users/swagnerc/phys_212/slurm_output/sampling.%N.%j.out # STDOUT
+#SBATCH -e /home/users/swagnerc/phys_212/slurm_output/sampling.%N.%j.err # STDERR
+
+python param_search.py ${1} ${2} ${3}

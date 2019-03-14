23170e60f3c492e00318d4606dae343a32e5458e
Sebastian Wagner-Carena <sebwagner@DN0a62c847.SUNet>
2019-03-13 19:22:48 -0700

    small typo in grid search code

--- grid_search.py
+++ grid_search.py
@@ -9,28 +9,28 @@ N_k = 30
 for lattice in lattices:
 	for nn in next_nearest:
 		if lattice == 'square':
-			if nn = False:
+			if nn == False:
 				Ks = np.concatenate((np.linspace(0.3,0.5,N_k),
 					np.linspace(0.4,0.45,N_k)))
 			else:
 				Ks = np.concatenate((np.linspace(0.02,0.2,N_k),
 					np.linspace(0.08,0.15,N_k)))
 		if lattice == 'triangular':
-			if nn = False:
+			if nn == False:
 				Ks = np.concatenate((np.linspace(0.2,0.4,N_k),
 					np.linspace(0.24,0.28,N_k)))
 			else:
 				s = np.concatenate((np.linspace(0.01,0.1,N_k),
 					np.linspace(0.06,0.08,N_k)))
 		if lattice == 'hexagonal':
-			if nn = False:
+			if nn == False:
 				Ks = np.concatenate((np.linspace(0.5,0.8,N_k),
 					np.linspace(0.65,0.67,N_k)))
 			else:
 				Ks = np.concatenate((np.linspace(0.05,0.3,N_k),
 					np.linspace(0.125,0.175,N_k)))
 		if lattice == 'random':
-			if nn = False:
+			if nn == False:
 				Ks = np.concatenate((np.linspace(0.2,0.5,N_k),
 					np.linspace(0.28,0.4,N_k)))
 			else:
